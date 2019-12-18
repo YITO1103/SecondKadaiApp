dec8b156eecb2aca3434d29e6fe8a4e1c9d89cbb
YITO1103 <yuta.j.1103@gmail.com>
2019-12-18 11:04:28 +0900

    ２画面の部品配置と遷移まで済み

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by user1 on 2019/12/18.
+//  Copyright © 2019 yutaka.ito4. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
