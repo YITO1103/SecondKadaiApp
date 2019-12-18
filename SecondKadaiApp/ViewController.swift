a4c314da2d931b0ace6c10fcfbbf83db2ce00356
YITO1103 <yuta.j.1103@gmail.com>
2019-12-18 10:43:50 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by user1 on 2019/12/18.
+//  Copyright © 2019 yutaka.ito4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
